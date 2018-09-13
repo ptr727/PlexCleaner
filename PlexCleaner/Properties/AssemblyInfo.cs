7f5e597624f906c67489fd5df8bb4b19555a19c6
Pieter Viljoen <pieter_viljoen@symantec.com>
2018-09-13 13:13:33 -0700

    Build from nuget

--- PlexCleaner/Properties/AssemblyInfo.cs
+++ PlexCleaner/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pieter Viljoen")]
 [assembly: AssemblyProduct("PlexCleaner")]
-[assembly: AssemblyCopyright("Copyright ©  2017 Pieter Viljoen")]
+[assembly: AssemblyCopyright("Copyright ©  2018 Pieter Viljoen")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
